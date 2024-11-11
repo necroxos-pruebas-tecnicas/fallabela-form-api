6ce579af3f710f7efd60b1728bdd62b225308735
Rodrigo Dorat <rdoratm@gmail.com>
2024-11-11 15:52:53 -0300

    :test_tube: UT for form answers

--- src/modules/form-answers/__tests__/form-answers.controller.spec.ts
+++ src/modules/form-answers/__tests__/form-answers.controller.spec.ts
@@ -1,20 +1,64 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FormAnswersController } from '../form-answers.controller';
 import { FormAnswersService } from '../form-answers.service';
+import { CreateFormAnswersDto } from '../dto';
 
 describe('FormAnswersController', () => {
   let controller: FormAnswersController;
+  let service: FormAnswersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FormAnswersController],
-      providers: [FormAnswersService],
+      providers: [
+        {
+          provide: FormAnswersService,
+          useValue: { create: jest.fn(), findByForm: jest.fn() },
+        },
+      ],
     }).compile();
 
     controller = module.get<FormAnswersController>(FormAnswersController);
+    service = module.get<FormAnswersService>(FormAnswersService);
   });
+
+  afterEach(() => jest.clearAllMocks());
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call create from service', async () => {
+      jest
+        .spyOn(service, 'create')
+        .mockImplementationOnce((createDto: CreateFormAnswersDto) => {
+          expect(createDto).toBeDefined();
+          expect(createDto).toHaveProperty('answers');
+          return null;
+        });
+
+      const createAnswers: CreateFormAnswersDto = {
+        answers: [],
+      };
+
+      await controller.create(createAnswers);
+    });
+  });
+
+  describe('findByForm', () => {
+    it('should call find from service and filter by fomrId', async () => {
+      const formId = 'form-1';
+
+      jest
+        .spyOn(service, 'findByForm')
+        .mockImplementationOnce((formId: string) => {
+          expect(formId).toBeDefined();
+          expect(formId).toBe(formId);
+          return null;
+        });
+
+      await controller.findByForm(formId);
+    });
   });
 });
--- src/modules/form-answers/__tests__/form-answers.service.spec.ts
+++ src/modules/form-answers/__tests__/form-answers.service.spec.ts
@@ -1,12 +1,27 @@
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+
 import { FormAnswersService } from '../form-answers.service';
+import { PrismaService } from '../../../services/prisma.service';
+import { CreateFormAnswersDto } from '../dto';
 
 describe('FormAnswersService', () => {
   let service: FormAnswersService;
+  let prismaMock: DeepMockProxy<PrismaClient>;
 
   beforeEach(async () => {
+    prismaMock = mockDeep<PrismaClient>();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FormAnswersService],
+      providers: [
+        FormAnswersService,
+        {
+          provide: PrismaService,
+          useValue: prismaMock,
+        },
+      ],
     }).compile();
 
     service = module.get<FormAnswersService>(FormAnswersService);
@@ -14,5 +29,64 @@ describe('FormAnswersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call createMany on Prisma with correct data', async () => {
+      const createFormAnswersDto: CreateFormAnswersDto = {
+        answers: [
+          { fieldId: 'field-1', value: 'answer-1' },
+          { fieldId: 'field-2', value: 'answer-2' },
+        ],
+      };
+
+      prismaMock.answer.createMany.mockResolvedValue({
+        count: 2,
+      });
+
+      const result = await service.create(createFormAnswersDto);
+
+      expect(prismaMock.answer.createMany).toHaveBeenCalledWith({
+        data: createFormAnswersDto.answers,
+      });
+      expect(result).toEqual({ count: 2 });
+    });
+  });
+
+  describe('findByForm', () => {
+    it('should return answers for a form by formId', async () => {
+      // Arrange: Datos de prueba
+      const formId = 'form-1';
+      const mockFields: any = [{ id: 'field-1' }, { id: 'field-2' }];
+
+      const mockAnswers: any = [
+        {
+          id: '1',
+          fieldId: 'field-1',
+          value: 'answer-1',
+          createdAt: new Date(),
+        },
+        {
+          id: '2',
+          fieldId: 'field-2',
+          value: 'answer-2',
+          createdAt: new Date(),
+        },
+      ];
+
+      prismaMock.field.findMany.mockResolvedValue(mockFields);
+      prismaMock.answer.findMany.mockResolvedValue(mockAnswers);
+
+      const result = await service.findByForm(formId);
+
+      expect(prismaMock.field.findMany).toHaveBeenCalledWith({
+        where: { formId },
+        select: { id: true },
+      });
+      expect(prismaMock.answer.findMany).toHaveBeenCalledWith({
+        where: { fieldId: { in: ['field-1', 'field-2'] } },
+      });
+      expect(result).toEqual(mockAnswers);
+    });
   });
 });
